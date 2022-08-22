8b5350f77c0d0d31ea40ea3fdb8292809ef825bd
Boueti <107395670+Boueti@users.noreply.github.com>
2022-08-22 13:03:13 +0200

    Add files via upload

---
+++ Basic Java/Ejercico 1/src/com/datos/DataType.java
@@ -0,0 +1,37 @@
+package com.datos;
+
+public class DataType {
+
+  public static void main(String[] args) {
+
+
+    // numerico
+    byte var1 = 1;
+    int var2 = 2;
+    short var9 = 9;
+    long var3 = 3;
+    float var4 = 4.7f;
+    double var5 = 5.8d;
+
+
+    // texto
+    char var6 = 'd';
+    String var7 = "Diego";
+
+    // booleano
+    boolean var8 = true;
+
+
+    // imprime por pantalla los tipos de variable
+    System.out.println(var1);
+    System.out.println(var2);
+    System.out.println(var3);
+    System.out.println(var4);
+    System.out.println(var5);
+    System.out.println(var6);
+    System.out.println(var7);
+    System.out.println(var8);
+
+
+  }
+}
